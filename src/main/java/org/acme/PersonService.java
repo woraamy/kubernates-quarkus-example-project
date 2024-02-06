5012c6db006a8866a32a70f3b556d2fff6ba7069
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-02-06 20:37:59 +0700

    Implement personresource

--- src/main/java/org/acme/PersonService.java
+++ src/main/java/org/acme/PersonService.java
@@ -2,31 +2,58 @@ package org.acme;
 
 import io.grpc.stub.StreamObserver;
 import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
 import org.acme.entity.Person;
-import org.acme.entity.grpc.PeopleServiceGrpc;
-import org.acme.entity.grpc.PersonList;
-import org.acme.entity.grpc.PersonResponse;
+import org.acme.entity.grpc.*;
+import org.acme.entity.grpc.Empty;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 @GrpcService
-public class PersonService extends PeopleServiceGrpc.PeopleServiceImplBase {
+public class PersonService implements PeopleService {
 
     @Override
-    public void listPersons(Empty request, StreamObserver<PersonList> responseObserver) {
-        List<Person> persons = Person.listAll();
-        List<PersonResponse> responses = persons.stream().map(p -> PersonResponse.newBuilder()
-                .setId(p.id.toString())
-                .setName(p.name)
-                .setBirth(p.birth.format(DateTimeFormatter.ISO_DATE))
-                .setStatus(p.status.toString())
-                .setAge(p.age)
-                .build()).collect(Collectors.toList());
-        PersonList response = PersonList.newBuilder().addAllPersons(responses).build();
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
+    public Uni<PersonList> listPersons(Empty request) {
+        return Uni.createFrom().item(() ->
+                PersonList.newBuilder().build()
+        );
+    }
+
+    @Override
+    public Uni<PersonResponse> getPerson(PersonIdRequest request) {
+        return Uni.createFrom().item(() ->
+                PersonResponse.newBuilder().build()
+        );
+    }
+
+    @Override
+    public Uni<PersonIdResponse> createPerson(PersonRequest request) {
+        return null;
+    }
+
+    @Override
+    public Uni<org.acme.entity.grpc.Empty> updatePerson(UpdatePersonRequest request) {
+        return null;
+    }
+
+    @Override
+    public Uni<org.acme.entity.grpc.Empty> deletePerson(PersonIdRequest request) {
+        return null;
+    }
+
+    @Override
+    public Uni<PersonResponse> searchPerson(SearchRequest request) {
+        return null;
+    }
+
+    @Override
+    public Uni<CountResponse> countPersons(org.acme.entity.grpc.Empty request) {
+        return Uni.createFrom().item(() ->
+                CountResponse.newBuilder().build()
+        );
     }
 }
