f9312a83df29a95d826ca4899e3c7acd776475fb
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-02-09 22:14:42 +0700

    Modify personservice java file

--- src/main/java/org/acme/PersonService.java
+++ src/main/java/org/acme/PersonService.java
@@ -3,8 +3,11 @@ package org.acme;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Uni;
 import org.acme.entity.Person;
+import org.acme.entity.Status;
 import org.acme.entity.grpc.*;
 import org.acme.entity.grpc.Empty;
+
+import java.time.LocalDate;
 
 
 @GrpcService
@@ -26,12 +29,19 @@ public class PersonService implements PeopleService {
                         .setId(person.id)
                         .setAge(person.age)
                         .setBirth(person.birth.toString())
+                        .setStatus(String.valueOf(person.status))
                         .build()
         );
     }
 
     @Override
     public Uni<PersonIdResponse> createPerson(PersonRequest request) {
+        Person person = new Person();
+        person.age = request.getAge();
+        person.name = request.getName();
+        person.birth = LocalDate.parse(request.getBirth());
+        person.status = Status.valueOf(request.getStatus());
+        person.persist();
         return Uni.createFrom().item(() ->
                 PersonIdResponse.newBuilder().build()
         );
@@ -39,6 +49,12 @@ public class PersonService implements PeopleService {
 
     @Override
     public Uni<Empty> updatePerson(UpdatePersonRequest request) {
+        Person person =  Person.findById(request.getId());
+        person.age = request.getPerson().getAge();
+        person.name = request.getPerson().getName();
+        person.birth = LocalDate.parse(request.getPerson().getBirth());
+        person.status = Status.valueOf(request.getPerson().getStatus());
+        person.persist();
         return Uni.createFrom().item(() ->
                 Empty.newBuilder().build()
         );
@@ -46,6 +62,8 @@ public class PersonService implements PeopleService {
 
     @Override
     public Uni<Empty> deletePerson(PersonIdRequest request) {
+        Person person =  Person.findById(request.getId());
+        person.delete();
         return Uni.createFrom().item(() ->
                 Empty.newBuilder().build()
         );
